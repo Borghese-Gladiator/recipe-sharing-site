739cbb6bf3f4841d54774ebbf66a254f31642ac4
Timothy Shee <timothy_shee@hotmail.com>
2020-10-11 03:12:20 -0400

    Updates

---
+++ precache-manifest.52b317a7d306024d8bafb090d4d63d1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df1ac1aa28251e2b9fcc24a094cba8e9",
+    "url": "/recipe-sharing-site/index.html"
+  },
+  {
+    "revision": "5f32403ddf500a1855e1",
+    "url": "/recipe-sharing-site/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "1ec9042db24c5c94ac4c",
+    "url": "/recipe-sharing-site/static/js/2.412be0d8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/recipe-sharing-site/static/js/2.412be0d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f32403ddf500a1855e1",
+    "url": "/recipe-sharing-site/static/js/main.6b50a326.chunk.js"
+  },
+  {
+    "revision": "2e0765eb926611a5c41b",
+    "url": "/recipe-sharing-site/static/js/runtime-main.8760cb3f.js"
+  }
+]);
