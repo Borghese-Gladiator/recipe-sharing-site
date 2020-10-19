6e7b234ed9f38c84fb0e936b8e35786f2ba97580
Timothy Shee <timothy_shee@hotmail.com>
2020-10-18 23:08:48 -0400

    Adjust sidebar icons and remove unused imports

--- src/App.js
+++ src/App.js
@@ -4,18 +4,16 @@ import {
   Route,
   BrowserRouter
 } from "react-router-dom";
-import { makeStyles } from '@material-ui/core/styles';
-
-import Toolbar from '@material-ui/core/Toolbar'
-import Paper from '@material-ui/core/Paper'
+// Custom menu: nav and sidebar
 import NavigationMenu from "./components/NavigationMenu"
+// Pages
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import RecipesPage from './pages/RecipesPage';
 import DashboardPage from './pages/DashboardPage';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import logo from './img/TastePerfect Logo.png'
-
+// Sidebar icons
 import HomeIcon from '@material-ui/icons/Home'
 import InfoIcon from '@material-ui/icons/Info';
 import FastfoodIcon from '@material-ui/icons/Fastfood';
@@ -33,7 +31,7 @@ export default function App(props) {
   const [lang, setLang] = React.useState('en');
 
   return (
-    <BrowserRouter initialEntries={['/drafts']} initialIndex={0}>
+    <HashRouter initialEntries={['/drafts']} initialIndex={0}>
       <div style={{backgroundColor: "#DAE3E7"}}>
         <NavigationMenu
           routeData={routeData}
@@ -46,6 +44,6 @@ export default function App(props) {
         <Route path="/recipes" component={RecipesPage} />
         <Route path="/dashboard" component={DashboardPage} />
       </div>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -1,14 +1,16 @@
 import React from "react";
+import SidebarDashboard from '../components/SidebarDashboard';
+
 import {
   Route,
   Link
 } from "react-router-dom";
-import SidebarDashboard from '../components/SidebarDashboard';
-
-import HomeIcon from '@material-ui/icons/Home'
-import InfoIcon from '@material-ui/icons/Info';
-import FastfoodIcon from '@material-ui/icons/Fastfood';
+import { makeStyles } from '@material-ui/core/styles';
+// Sidebar icons
 import DashboardIcon from '@material-ui/icons/Dashboard';
+import PeopleIcon from '@material-ui/icons/People';
+import HistoryIcon from '@material-ui/icons/History';
+import AccountBoxIcon from '@material-ui/icons/AccountBox';
 
 function HomeDashboard() {
   return (
@@ -35,27 +37,30 @@ function SocialDashboard() {
     </div>
   )
 }
-
-
+// declared here && SidebarDashboard - unable to pass since SidebarDashboard drawerWidth is inside useStyles()
+const drawerWidth = 240;
 
 export default function DashboardPage({ match: { url } }) {
   const routeData = [
-    { text: 'HOME', link: `${url}/home`, iconFunc: () => { return <HomeIcon /> } },
-    { text: 'PROFILE', link: `${url}/profile`, iconFunc: () => { return <InfoIcon /> } },
-    { text: 'HISTORY', link: `${url}/history`, iconFunc: () => { return <FastfoodIcon /> } },
-    { text: 'SOCIAL', link: `${url}/`, iconFunc: () => { return <DashboardIcon /> } },
+    { text: 'DASHBOARD', link: `${url}/home`, iconFunc: () => { return <DashboardIcon /> } },
+    { text: 'PROFILE', link: `${url}/profile`, iconFunc: () => { return <AccountBoxIcon /> } },
+    { text: 'HISTORY', link: `${url}/history`, iconFunc: () => { return <HistoryIcon /> } },
+    { text: 'SOCIAL', link: `${url}/social`, iconFunc: () => { return <PeopleIcon /> } },
   ]
   
   return (
     <div style={{backgroundColor: "#DAE3E7"}}>
       <SidebarDashboard
+        drawerWidth={drawerWidth}
         routeData={routeData}
       />
-      <Route path={`${url}/home`} component={HomeDashboard} />
-      <Route path={`${url}/profile`} component={ProfileDashboard} />
-      <Route path={`${url}/history`} component={HistoryDashboard } />
-      <Route path={`${url}/social`} component={SocialDashboard} />
-      <Route path={`${url}/`} component={HomeDashboard} exact />
+      <main style={{marginLeft: drawerWidth}}>
+        <Route path={`${url}/home`} component={HomeDashboard} />
+        <Route path={`${url}/profile`} component={ProfileDashboard} />
+        <Route path={`${url}/history`} component={HistoryDashboard } />
+        <Route path={`${url}/social`} component={SocialDashboard} />
+        <Route path={`${url}/`} component={HomeDashboard} exact />
+      </main>
     </div>
   );
 }
