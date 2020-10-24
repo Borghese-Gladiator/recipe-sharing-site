c6077a4497360ad02a0b481785b46f0a3bc3767b
Timothy Shee <timothy_shee@hotmail.com>
2020-10-24 02:43:59 -0400

    Update recipeData.js

--- src/constants/recipeData.js
+++ src/constants/recipeData.js
@@ -71,4 +71,76 @@ export const recipeData = [
     desc: "",
     user: "Chef John"
   },
+  {
+    name: "Mapo Tofu",
+    imgPath: "foodImages/chinese/mapo-tofu-recipe-6154w.jpg",
+    stars: 5, 
+    tags: ["Chinese", "Spicy"],
+    ingredients: ["Tofu", "豆瓣酱", "Ground meat", "Salt"],
+    desc: "",
+    user: "Chef John"
+  },
+  {
+    name: "Mapo Tofu",
+    imgPath: "foodImages/chinese/mapo-tofu-recipe-6154w.jpg",
+    stars: 5, 
+    tags: ["Chinese", "Spicy"],
+    ingredients: ["Tofu", "豆瓣酱", "Ground meat", "Salt"],
+    desc: "",
+    user: "Chef John"
+  },
+  {
+    name: "Mapo Tofu",
+    imgPath: "foodImages/chinese/mapo-tofu-recipe-6154w.jpg",
+    stars: 4, 
+    tags: ["Chinese", "Spicy"],
+    ingredients: ["Tofu", "豆瓣酱", "Ground meat", "Salt"],
+    desc: "",
+    user: "Chef John"
+  },
+  {
+    name: "Mapo Tofu",
+    imgPath: "foodImages/chinese/mapo-tofu-recipe-6154w.jpg",
+    stars: 5, 
+    tags: ["Chinese", "Spicy"],
+    ingredients: ["Tofu", "豆瓣酱", "Ground meat", "Salt"],
+    desc: "",
+    user: "Chef John"
+  },
+  {
+    name: "Mapo Tofu",
+    imgPath: "foodImages/chinese/mapo-tofu-recipe-6154w.jpg",
+    stars: 5, 
+    tags: ["Chinese", "Spicy"],
+    ingredients: ["Tofu", "豆瓣酱", "Ground meat", "Salt"],
+    desc: "",
+    user: "Chef John"
+  },
+  {
+    name: "Mapo Tofu",
+    imgPath: "foodImages/chinese/mapo-tofu-recipe-6154w.jpg",
+    stars: 5, 
+    tags: ["Chinese", "Spicy"],
+    ingredients: ["Tofu", "豆瓣酱", "Ground meat", "Salt"],
+    desc: "",
+    user: "Chef John"
+  },
+  {
+    name: "Mapo Tofu",
+    imgPath: "foodImages/chinese/mapo-tofu-recipe-6154w.jpg",
+    stars: 5, 
+    tags: ["Chinese", "Spicy"],
+    ingredients: ["Tofu", "豆瓣酱", "Ground meat", "Salt"],
+    desc: "",
+    user: "Chef John"
+  },
+  {
+    name: "Mapo Tofu",
+    imgPath: "foodImages/chinese/mapo-tofu-recipe-6154w.jpg",
+    stars: 5, 
+    tags: ["Chinese", "Spicy"],
+    ingredients: ["Tofu", "豆瓣酱", "Ground meat", "Salt"],
+    desc: "",
+    user: "Chef John"
+  },
 ]
